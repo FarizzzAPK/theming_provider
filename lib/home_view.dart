1017359eba45148251505251915b7d4d2d98865e
Farizzz <113644816+FarizzzAPK@users.noreply.github.com>
2025-12-14 13:58:39 +0200

    counter_provider_finished

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:theming_provider/provider/counter_provider.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    int counter = Provider.of<CounterProvider>(context).counter;
+    return Scaffold(
+      appBar: AppBar(title: const Text("simple counter app")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("$counter",style: TextStyle(
+              fontSize: 25
+                ),)
+            
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+                            ElevatedButton(onPressed: () => Provider.of<CounterProvider>(context,listen: false).decrement(), child: Icon(Icons.remove)),
+
+              ElevatedButton(onPressed: () => Provider.of<CounterProvider>(context,listen: false).increment(), child: Icon(Icons.add)),
+            
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:theming_provider/home_view.dart';
+import 'package:theming_provider/provider/counter_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-debugShowCheckedModeBanner: false,    
-      home: HomeView(),
+    return ChangeNotifierProvider <CounterProvider>(
+      create: (context) => CounterProvider(),
+      child: MaterialApp(
+      debugShowCheckedModeBanner: false,    
+        home: HomeView(),
+      ),
     );
   }
 }
